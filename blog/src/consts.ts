5bbee98a032ade0b09f9bfb9a7213724f5438c62
Jimmy B. <chips_xp@yahoo.com>
2025-01-04 01:28:37 -0500

    Add Image props, Research Advance posts, all links in link.ts

--- blog/src/consts.ts
+++ blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ChipsXP Research';
+export const SITE_DESCRIPTION = 'ChipsXP Research is a blog about the latest research in computer language, machine learning, and artificial intelligence.';
