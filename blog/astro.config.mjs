2c892a93d1b9e08caa7ff504369c997cf5550d97
Jimmy Burns (pluckCode) <chips_xp@yahoo.com>
2025-06-11 21:50:32 -0400

    removed SSR and add formspree server to make static

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://chipsxp.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static", // Change from "server" to "static", Astro default, but astro's server node is integrated
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static", // This ensures a fully static build
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
